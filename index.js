fb22129ddd1ad9408b66c78de434e68fa51cadf0
JacobK <94710630+Jacobkyalo@users.noreply.github.com>
2022-08-16 11:56:16 -0700

    added comments

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+//grabbing the checkbox from the DOM
 const checkbox = document.getElementById("checkbox");
 
+//adding eventListener to the checkbox
 checkbox.addEventListener("change", () => {
   document.body.classList.toggle("dark");
 });
